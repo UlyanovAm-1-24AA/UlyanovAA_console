671035ebb7e33f87f7dc4fb98b893da07ca6a2c8
UlyanovAA <ulyanovalexsandr@mail.ru>
2025-06-01 18:09:15 +0300

    Add *.cs-files

---
+++ New_Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UlyanovAA
+{
+    internal class New_Class2
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UlyanovAA
+{
+    internal class Program
+    {
+        static public double Sqr(double X)
+        {
+            return X * X;
+        }
+
+        static void TestSqr()
+        {
+            (double input, double expected)[] tests = new[]
+            {
+            (2.0, 4.0),
+            (-3.0, 9.0),
+            (0.0, 0.0),
+            (1.5, 2.25),
+            (0.1, 0.01)
+            };
+
+            foreach (var test in tests)
+            {
+                double result = Sqr(test.input);
+                bool passed = Math.Abs(result - test.expected) < 1e-10; // Учитываем погрешность для дробных чисел
+                Console.WriteLine($"Вход: {test.input}, Ожидаем: {test.expected}, Вывод: {result}, {(passed ? "+" : "-")}");
+            }
+        }
+
+        static void Main()
+        {
+            TestSqr();
+        }
+    }
+}
+//UlyanovAA Am-1-24
+
+
